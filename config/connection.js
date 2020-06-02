64c4ff1dd99c02641cadae252c6340519b246650
Lachlan Mckellar <lachlan.person@gmail.com>
2020-06-02 21:25:08 +1000

    a

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,29 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-var connection;
+// // Set up MySQL connection.
+// var mysql = require("mysql");
+// var connection;
 
-if (process.env.JAWSDB_URL) {
-  console.log("PRODUCTION")
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+// if (process.env.JAWSDB_URL) {
+//   console.log("PRODUCTION")
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
  
-} else {
-  console.log("DEV")
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Mckellar12!',
-    database: 'planner_db'
-  });
-}
+// } else {
+//   console.log("DEV")
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Mckellar12!',
+//     database: 'planner_db'
+//   });
+// }
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+// // Export connection for our ORM to use.
+// module.exports = connection;
